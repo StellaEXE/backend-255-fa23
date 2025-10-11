299b9cb5a988ca933579a2e962bf4cae53aebccf
StellaEXE <ikpeterson18@gmail.com>
2025-10-10 20:17:14 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-<<<<<<< HEAD
 const express = require("express");
 const Song = require("./models/songs");
 var cors = require('cors')
+//const bodyParser = require('body-parser')
+const jwt = require('jwt-simple')
+const User = require("./models/users")
 
 const app = express();
 app.use(cors())
@@ -10,6 +12,92 @@ app.use(cors())
 app.use(express.json());
 
 const router = express.Router();
+const secret = "supersecret"
+
+//creating a new user
+router.post("/user", async (req, res) => {
+    if (!req.body.username || !req.body.password) {
+        res.status(400).json({ error: "Missing username or password" })
+    }
+
+    const newUser = await new User({
+        username: req.body.username,
+        password: req.body.password,
+        status: req.body.status
+
+    })
+
+    try {
+        await newUser.save()
+        console.log(newUser)
+        res.status(201) //created
+    }
+    catch (err) {
+        res.sendStatus(400).send(err)
+    }
+
+})
+
+//authenticate or login
+//post request - reason why is because when you login you are cerating what we call a new 'session'
+router.post("/auth", async (req, res) => {
+    if (!req.body.username || !req.body.password) {
+        res.status(400).json({ error: "Missing username or password" })
+        return
+    }
+    //try to find the username in the database, then see if it matches with a username and password
+    //await finding a user
+    let user = await User.findOne({ username: req.body.username })
+
+    if (!user) {
+        res.status(401).json({ error: "Bad Username" })
+    }
+    //check to see if the users password matches the requests password
+    else {
+        if (user.password != req.body.password) {
+            res.status(401).json({ error: "Bad Password" })
+        }
+        //successful login
+        else {
+            //create a token that is encoded with the jwt library, and sent back the username... this will be important later
+            //we also will send back as part of the token that you are current authorized
+            //we could do this with a boolean or a number value i.e if auth = 0 you are not authorized, if auth
+            // equals 1 you are authorized
+
+            username2 = user.username
+            const token = jwt.encode({ username: user.username }, secret)
+            const auth = 1
+
+            //respond with the token
+            res.json({
+                username2,
+                token: token,
+                auth: auth
+            })
+        }
+    }
+})
+
+
+//check status of user with a valid token, see if it matches the front end token
+router.get("/status", async (req, res) => {
+    if (!req.headers["x-auth"]) {
+        return res.status(401).json({ error: "Missing X-Auth" })
+    }
+
+    //if x-auth contains the token (it should)
+    const token = req.headers["x-auth"]
+    try {
+        const decoded = jwt.decode(token, secret)
+
+        //send back all username and status fields to the user or front end
+        let users = User.find({}, "username status")
+        res.json(users)
+    }
+    catch (ex) {
+        res.status(401).json({ error: "invalid jwt" })
+    }
+})
 
 //Get list of all songs in the database
 router.get("/songs", async (req, res) => {
@@ -20,6 +108,7 @@ router.get("/songs", async (req, res) => {
     }
     catch (err) {
         console.log(err)
+        res.status(400).send(err)
     }
 
 })
@@ -67,42 +156,22 @@ router.put("/songs/:id", async (req, res) => {
     }
 })
 
-=======
-//setup.. this is similar to when we use our default tags in html
-const express = require("express")
-//we have to use cors in order to host a front end and backend on the same device
-var cors = require('cors')
-//activate or tell this app variable to be an express server
-const app = express()
-app.use(cors())
-const router = express.Router()
+router.delete("/songs/:id", async (req, res) => {
+    //method or function in mongoose/mongo to delete a single instance of a song or object
+    try {
+        const song = await Song.findById(req.params.id)
+        console.log(song)
+        await Song.deleteOne({ _id: song._id })
+        res.sendStatus(204)
+    }
 
-//making an api using routes
-// Routes are used to handle browser requests. They look like URLs. The difference is that when a browser requests a route, it is dynamically handled by using a function.
-
-router.get("/songs", function(req,res){
-    const songs = [
-        {
-            title: "We Found Love",
-            artist: "Rihanna",
-            popularity: 10,
-            releaseDate: new Date(2011, 9, 22),
-            genre: ["electro house"]
-        },
-        {
-            title: "Happy",
-            artist: "Pharell Williams",
-            popularity: 10,
-            releaseDate: new Date(2013, 11, 21),
-            genre: ["soul", "new soul"]
-        }
-    ];
-
-
-    res.json(song)
+    catch (err) {
+        res.status(400).send(err)
+    }
 })
 
-//all requests that usually use an api start with /api... so the url would be localhost:3000/api/songs
->>>>>>> f1a74b878f271689643673a215b7dc39a580973f
-app.use("/api", router)
-app.listen(3000)
+app.use("/api", router);
+
+var port = process.env.PORT || 3000
+
+app.listen(port);
